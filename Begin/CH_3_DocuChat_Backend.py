13b13f5c09f613f9d91f7f907098161360eaffe7
sapubob <sapubob@gmail.com>
2025-05-20 14:19:45 -0500

    Update CH_3_DocuChat_Backend.py

--- Begin/CH_3_DocuChat_Backend.py
+++ Begin/CH_3_DocuChat_Backend.py
@@ -75,7 +75,6 @@ try:
 except:
     # Handle exceptions and print detailed error information
     print(traceback.format_exc())
-
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     # Print information about the exception type, filename, and line number
